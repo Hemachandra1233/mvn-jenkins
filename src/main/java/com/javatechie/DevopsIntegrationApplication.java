6c6819234ded6098598e8303b483b47b60052da1
Hemachandra C <hemachandra.chindam@argano.com>
2024-03-13 18:40:51 +0530

    test

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -1,22 +1,33 @@
 package com.javatechie;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @RestController
 public class DevopsIntegrationApplication {
 
-	@GetMapping
-	public String message(){
-		return "welcome to javatechie";
-	}
-
-	@GetMapping("/hello")
-	public String message2(){
-		return "Hello World Swap Hema";
+	@Autowired
+	PersonsRepository repo; 
+	
+	@GetMapping("/persons")
+	public List<Persons> getPersons(){
+		List<Persons> result = new ArrayList<Persons>();
+		Iterable<Persons> iterable = repo.findAll();
+		Iterator<Persons> iter = iterable.iterator();
+		while(iter.hasNext()) {
+			result.add(iter.next());
+		}
+		return result;
 	}
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/javatechie/Persons.java
@@ -0,0 +1,44 @@
+package com.javatechie;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(schema = "testdb", name="Persons")
+public class Persons {
+
+	@Id
+	private Integer id;
+	private String lastName;
+	private String firstName;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+}
